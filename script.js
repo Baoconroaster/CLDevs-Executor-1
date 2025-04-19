5989f8c068a55ddbac2fb05d3792b400cfb38bac
BaconRoaster <bacondiscord0@gmail.com>
2025-04-19 10:58:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Add any JavaScript functionality here if needed
+document.addEventListener('DOMContentLoaded', function() {
+    // The site is currently static, but you can add dynamic features here
+});
